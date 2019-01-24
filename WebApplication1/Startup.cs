8494986214462adefd9241c3ee406643f40bae44
Raimonds Raginskis <S00132263@mail.itsligo.ie>
2019-01-24 21:05:01 +0000

    test

--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -23,11 +23,12 @@ namespace WebApplication1
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseStaticFiles();
             }
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello World from Raimonds!");
             });
         }
     }

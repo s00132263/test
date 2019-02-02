eb87bdfac912162e1881a6a449eec9ba781f4448
Raimonds Raginskis <S00132263@mail.itsligo.ie>
2019-02-02 10:26:07 +0000

    Push1

--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -15,6 +15,7 @@ namespace WebApplication1
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -23,8 +24,11 @@ namespace WebApplication1
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseStaticFiles();
+               
             }
+
+            app.UseStaticFiles();
+            app.UseMvc();
 
             app.Run(async (context) =>
             {
